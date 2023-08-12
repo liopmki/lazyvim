5ad1c98e31492d11e964e8e70dbddcab16e07501
liopmki <liuyiy4ng@outlook.com>
2023-08-12 15:25:51 +0800

    add my config

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    cmd = "Neorg",
+    ft = "norg",
+    dependencies = { "nvim-lua/plenary.nvim", lazy = true },
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              -- notes = "~/notes",
+              notes = "C:\\Users\\Administrator\\OneDrive\\Project\\notes\\neorg",
+            },
+          },
+          default_workspace = "notes",
+        },
+        ["core.summary"] = {}, -- generate Summary
+      },
+    },
+  },
+}
