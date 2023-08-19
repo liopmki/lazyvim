53496db3213a570742e26b5d11b6fa7a1fecab0a
liopmki <liuyiy4ng@outlook.com>
2023-08-19 21:55:54 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.scrolloff = 8
+
 -- [[[ Neovide ]]]
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
