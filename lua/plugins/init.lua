304d4ca338514bad591811f205fc6a757cf6c844
liopmki <liuyiy4ng@outlook.com>
2024-08-23 16:27:03 +0800

    Enable LazyVim Extra `Toml`

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" }, -- Typescript
   { import = "lazyvim.plugins.extras.lang.clangd" }, -- C and Cpp
   { import = "lazyvim.plugins.extras.lang.nushell" }, -- Nushell
+  { import = "lazyvim.plugins.extras.lang.toml" }, -- Toml
   -- colorscheme
   {
     "LazyVim/LazyVim",
