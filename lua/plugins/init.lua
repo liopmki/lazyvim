5eb3e7a5eac19717dcd72808d89917bcac81cfa5
liopmki <liuyiy4ng@outlook.com>
2024-08-23 16:25:30 +0800

    Enable LazyVim Extra Nushell

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   -- LazyVim Extras
   { import = "lazyvim.plugins.extras.lang.typescript" }, -- Typescript
   { import = "lazyvim.plugins.extras.lang.clangd" }, -- C and Cpp
+  { import = "lazyvim.plugins.extras.lang.nushell" }, -- Nushell
   -- colorscheme
   {
     "LazyVim/LazyVim",
