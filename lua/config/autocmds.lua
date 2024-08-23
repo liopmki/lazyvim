10ffe33a2cc83157b6f495c707ba336e0192f457
liopmki <liuyiy4ng@outlook.com>
2024-08-23 16:49:46 +0800

    Add Neovide Gui config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,25 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Neovide IME config
+local function set_ime(args)
+  if args.event:match("Enter$") then
+    vim.g.neovide_input_ime = true
+  else
+    vim.g.neovide_input_ime = false
+  end
+end
+
+local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+  group = ime_input,
+  pattern = "*",
+  callback = set_ime,
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+  group = ime_input,
+  pattern = "[/\\?]",
+  callback = set_ime,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Neovide
+if vim.g.neovide then
+  vim.g.neovide_padding_top = 2
+  vim.g.neovide_padding_bottom = 2
+  vim.g.neovide_padding_right = 2
+  vim.g.neovide_padding_left = 2
+  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_input_ime = false
+end
